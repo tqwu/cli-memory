80d39a9ec5892b5ed9cd2c1130d88da8a68273d2
Teresa <tqwu@ucsc.edu>
2025-01-16 16:41:49 -0800

    Adding command validation

--- memory.py
+++ memory.py
@@ -1,17 +1,37 @@
 
+import sys
+import os
+
+def handleGet(filename):
+    print(filename)
+
+def handleSet(filename):
+    print(filename)
+
 def main():
+    
     instructions = """
     This CLI supports two functions:
     - GET : retrieve the contents of an existing file
     - SET : create and set the contents of a new file OR update the contents of an existing file
     """
+    
     print(instructions)
-    command = input("Please enter either 'GET' or 'SET': ")
-    print("You entered: ", command)
-    filename = input("Please enter your filename: ")  # TODO: retrieve file contents of a file outside current directory?
-    print("You entered: ", filename)
-    if command == "SET":
+    
+    command = input("Please enter either 'GET' or 'SET': ").upper()
+    if command != "GET" and command != "SET":
+        sys.exit("\nInvalid command entered. Exiting program...")
+
+    filename = input("Please enter your filename: ")
+
+    if command == "GET":
+        if not os.path.isfile(filename):
+            sys.exit("\nFile does not exist. Exiting program...")
+        handleGet(filename)
+        
+    elif command == "SET":
         print("Please enter the contents of your file: ")
+        handleSet(filename)
 
 if __name__ == "__main__":
     main()
